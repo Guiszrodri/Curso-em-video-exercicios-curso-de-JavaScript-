e43660105aa49db255187093e73cee6732e21bdb
guiszrodri <guiszrodri12@gmail.com>
2023-01-02 14:38:06 -0300

    exercicio 5 a 13

---
+++ Exe008.js
@@ -0,0 +1,6 @@
+var vel = 40
+console.log(`A velocidade so seu carro é ${vel}KM/h`)
+if(vel>60){ //condição simple
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre com cinto de segurança')
---
+++ exe011.js
@@ -0,0 +1,9 @@
+var idade = 60
+if(idade<16) {
+    console.log('Não Volta')
+} else if (idade < 18 || idade > 65) {
+        console.log('Voto Opcional')
+} else {
+        console.log('Voto Obrigatório!')
+}
+    
